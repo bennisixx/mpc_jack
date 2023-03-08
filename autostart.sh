02d99cb72308cffdc16df19750ae41710ad00d3f
benfurstenwerth <benfurstenwerth@gmail.com>
2023-03-08 17:35:00 -0600

    ip address getter

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,7 @@
+ip=$(ip a | grep global | awk '{print $2;}' | sed -n 's/\/.*//p')
+echo $ip
 jackd -d alsa -d hw:ACVA &
-zita-j2n 10.42.6.66 8888 &
+./zita-j2n $ip 8888 &
 jack_connect system:capture_1 zita-j2n:in_1 &
 jack_connect system:capture_2 zita-j2n:in_2 &
 
